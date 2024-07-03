9467c78c01ca227712b8efc1e41734d4c74d15bf
julian2bot <marquesjulian26@gmail.com>
2024-07-03 20:11:22 +0200

    correction bug + peaufiner page skill

--- files/skill.php
+++ files/skill.php
@@ -23,103 +23,93 @@
             <script src="../animation/script/main.js"></script>
             <div class="card-container">
                 <div class="card">
-                    <img src="../asset/images/Python.png" alt="arduino">
-                    <div class="content">
-                        <h3>Python</h3>
-                        <p>IA Pacman python, gestion de donnée, gestion serveur...  </p>
-                        <progress max="100" value="80"></progress>
+                    <img   src="../asset/images/Python.png" alt="arduino">
+                    
+                    <h3>Python</h3>
+                    <p>IA Pacman python, gestion de donnée, gestion serveur...  </p>
+                    <progress max="100" value="80"></progress>
 
-                    </div>
+                    
                 </div>
                 <div class="card">
-                    <img src="../asset/images/Html.png" alt="arduino">
-                    <div class="content">
-                        <h3>Html</h3>
-                        <p>Site Internet... </p>
-                        <progress max="100" value="60"></progress>
+                    <img src="../asset/images/html.png" alt="logo htl">
+                
+                    <h3>Html</h3>
+                    <p>Site Internet... </p>
+                    <progress max="100" value="60"></progress>
 
-                    </div>
                 </div>
                 <div class="card">
-                    <img src="../asset/images/base-de-donnees.png" alt="arduino">
-                    <div class="content">
-                        <h3>Base de donnees</h3>
-                        <p>Gestion de base de donnée, communication avec base de donnée...</p>
-                        <progress max="100" value="55"></progress>
+                    <img src="../asset/images/base-de-donnees.png" alt="logo base de donnée">
+                
+                    <h3>Base de donnees</h3>
+                    <p>Gestion de base de donnée, communication avec base de donnée...</p>
+                    <progress max="100" value="55"></progress>
 
-                    </div>
                 </div>
                 <div class="card">
-                    <img src="../asset/images/Css.png" alt="arduino">
-                    <div class="content">
-                        <h3>Css</h3>
-                        <p>styliser des pages, animations page...</p>
-                        <progress max="100" value="70"></progress>
+                    <img src="../asset/images/css.png" alt="logo css">
+                
+                    <h3>Css</h3>
+                    <p>styliser des pages, animations page...</p>
+                    <progress max="100" value="70"></progress>
 
-                    </div>
                 </div>
                 <div class="card">
-                    <img src="../asset/images/php.png" alt="arduino">
-                    <div class="content">
-                        <h3>PHP</h3>
-                        <p>Gestion serveur client, simplifier des pages...</p>
-                        <progress max="100" value="30"></progress>
+                    <img src="../asset/images/php.png" alt="logo php">
+                
+                    <h3>PHP</h3>
+                    <p>Gestion serveur client, simplifier des pages...</p>
+                    <progress max="100" value="30"></progress>
 
-                    </div>
                 </div>
                 <div class="card">
-                    <img src="../asset/images/JavaScript.png" alt="arduino">
-                    <div class="content">
-                        <h3>JavaScript</h3>
-                        <p>Gestion client sur site internet, Bot discord.Js, jeu video, nique JS </p>
-                        <progress max="100" value="10"></progress>
+                    <img src="../asset/images/javascript.png" alt="logo JavaScript">
+                
+                    <h3>JavaScript</h3>
+                    <p>Gestion client sur site internet, Bot discord.Js, jeu video, nique JS </p>
+                    <progress max="100" value="10"></progress>
 
-                    </div>
                 </div>
                 <div class="card">
-                    <img src="../asset/images/arduino.png" alt="arduino">
-                    <div class="content">
-                        <h3>Arduino</h3>
-                        <p>Création feux circulation, robot reveil, gestion heure, gestion materiel & logiciel</p>
-                        <progress max="100" value="75"></progress>
+                    <img src="../asset/images/arduino.png" alt="logo arduino">
+                
+                    <h3>Arduino</h3>
+                    <p>Création feux circulation, robot reveil, gestion heure, gestion materiel & logiciel</p>
+                    <progress max="100" value="75"></progress>
 
-                    </div>
                 </div>
                 <div class="card">
-                    <img src="../asset/images/Java.png" alt="arduino">
-                    <div class="content">
-                        <h3>Java</h3>
-                        <p>Implémentation back & front end d'un logiciel</p>
-                        <progress max="100" value="60"></progress>
+                    <img src="../asset/images/java.png" alt="logo java">
+                
+                    <h3>Java</h3>
+                    <p>Implémentation back & front end d'un logiciel</p>
+                    <progress max="100" value="60"></progress>
 
-                    </div>
                 </div>
                 <div class="card">
-                    <img src="../asset/images/SolidWorks.png" alt="arduino">
-                    <div class="content">
-                        <h3>SolidWorks / 3D</h3>
-                        <p>Modelisation & impression piece technique et figurine</p>
-                        <progress max="100" value="60"></progress>
+                    <img src="../asset/images/solidworks.png" alt="logo solidworks">
+                
+                    <h3>SolidWorks / 3D</h3>
+                    <p>Modelisation & impression piece technique et figurine</p>
+                    <progress max="100" value="60"></progress>
 
-                    </div>
                 </div>
                 <div class="card">
-                    <img src="../asset/images/SolidWorks.png" alt="arduino">
-                    <div class="content">
-                        <h3>Modélisme</h3>
-                        <p>Voiture RC & Drone Fpv à réparer </p>
-                        <progress max="100" value="90"></progress>
+                    <img src="../asset/images/solidworks.png" alt="image qui represente le modélisme">
+                
+                    <h3>Modélisme</h3>
+                    <p>Voiture RC & Drone Fpv à réparer </p>
+                    <progress max="100" value="90"></progress>
 
-                    </div>
                 </div>
                 <div class="card">
-                    <img src="../asset/images/circuit-electrique.png" alt="arduino">
-                    <div class="content">
-                        <h3>Conception schéma electrique</h3>
-                        <p>creation de schéma electrique + realisation schéma electrique</p>
-                        <progress max="100" value="80"></progress>
+                    <img src="../asset/images/circuit-electrique.png" alt="image qui represente un circuit electrique">
+                
+                    <h3>Conception schéma electrique</h3>
+                    <p>creation de schéma electrique + realisation schéma electrique</p>
+                    <progress max="100" value="80"></progress>
 
-                    </div>
                 </div>
             </div>
            

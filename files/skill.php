6dff9837118a91ee505d008fa043bd90125c55c4
julian2bot <marquesjulian26@gmail.com>
2024-07-04 00:11:05 +0200

    projet + skill fini dans les textes + mise en page

--- files/skill.php
+++ files/skill.php
@@ -1,122 +1,157 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>About me</title>
-    <link rel="stylesheet" href="../style/global.css">
-    <link rel="stylesheet" href="../style/style.css">  
-    <link rel="stylesheet" href="../animation/style/animation.css">
-</head>
-<body>
-    <?php
-        include __DIR__ . '/../affichage/animation.php';
-        include __DIR__ . '/../affichage/animationSousPage.php';
-    ?>
-    
-    <script src="../animation/script/animationSousPage.js"></script>
-    
-    <div class="body">
-        <section>
-            <?php include __DIR__ . '/../affichage/header.php'; ?>
-            <h1>Compétences</h1>
-            <script src="../animation/script/main.js"></script>
-            <div class="card-container">
-                <div class="card">
-                    <img   src="../asset/images/Python.png" alt="arduino">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>About me</title>
+        <link rel="stylesheet" href="../style/global.css">
+        <link rel="stylesheet" href="../style/style.css">  
+        <link rel="stylesheet" href="../animation/style/animation.css">
+    </head>
+    <body>
+        <?php
+            include __DIR__ . '/../affichage/animation.php';
+            include __DIR__ . '/../affichage/animationSousPage.php';
+        ?>
+        
+        <script src="../animation/script/animationSousPage.js"></script>
+        
+        <div class="body">
+            <section>
+                <?php include __DIR__ . '/../affichage/header.php'; ?>
+                
+                <script src="../animation/script/main.js"></script>
+
+                <h1>Compétences</h1>            
+                <div class="card-container">
                     
-                    <h3>Python</h3>
-                    <p>IA Pacman python, gestion de donnée, gestion serveur...  </p>
-                    <progress max="100" value="80"></progress>
+                    <div class="card">
+                        <img   src="../asset/images/Python.png" alt="arduino">
+                        
+                        <h2>Python</h2>
+                        <p>
+                            IA Pacman, gestion de données, gestion de serveur.
+                        </p>
+                        <progress max="100" value="80"></progress>
 
+                        
+                    </div>
                     
-                </div>
-                <div class="card">
-                    <img src="../asset/images/html.png" alt="logo htl">
-                
-                    <h3>Html</h3>
-                    <p>Site Internet... </p>
-                    <progress max="100" value="60"></progress>
+                    <div class="card">
+                        <img src="../asset/images/html.png" alt="logo htl">
+                    
+                        <h2>Html</h2>
+                        <p>
+                            Site internet vitrine et dynamique.
+                        </p>
+                        <progress max="100" value="60"></progress>
 
-                </div>
-                <div class="card">
-                    <img src="../asset/images/base-de-donnees.png" alt="logo base de donnée">
-                
-                    <h3>Base de donnees</h3>
-                    <p>Gestion de base de donnée, communication avec base de donnée...</p>
-                    <progress max="100" value="55"></progress>
+                    </div>
+                    
+                    <div class="card">
+                        <img src="../asset/images/base-de-donnees.png" alt="logo base de donnée">
+                    
+                        <h2>Base de données</h2>
+                        <p>
+                            Gestion de bases de données, communication avec bases de données.
+                        </p>
+                        <progress max="100" value="55"></progress>
 
-                </div>
-                <div class="card">
-                    <img src="../asset/images/css.png" alt="logo css">
-                
-                    <h3>Css</h3>
-                    <p>styliser des pages, animations page...</p>
-                    <progress max="100" value="70"></progress>
+                    </div>
+                    
+                    <div class="card">
+                        <img src="../asset/images/css.png" alt="logo css">
+                    
+                        <h2>Css</h2>
+                        <p>
+                            Stylisation de pages, animations de page.
+                        </p>
+                        <progress max="100" value="70"></progress>
 
-                </div>
-                <div class="card">
-                    <img src="../asset/images/php.png" alt="logo php">
-                
-                    <h3>PHP</h3>
-                    <p>Gestion serveur client, simplifier des pages...</p>
-                    <progress max="100" value="30"></progress>
+                    </div>
+                    
+                    <div class="card">
+                        <img src="../asset/images/php.png" alt="logo php">
+                    
+                        <h2>PHP</h2>
+                        <p>
+                            Gestion serveur-client, simplification de pages.
+                        </p>
+                        <progress max="100" value="30"></progress>
 
-                </div>
-                <div class="card">
-                    <img src="../asset/images/javascript.png" alt="logo JavaScript">
-                
-                    <h3>JavaScript</h3>
-                    <p>Gestion client sur site internet, Bot discord.Js, jeu video, nique JS </p>
-                    <progress max="100" value="10"></progress>
+                    </div>
+                    
+                    <div class="card">
+                        <img src="../asset/images/javascript.png" alt="logo JavaScript">
+                    
+                        <h2>JavaScript</h2>
+                        <p>
+                            Gestion client sur site internet, Bot Discord.js, développement de jeux vidéo.
+                        </p>
+                        <progress max="100" value="10"></progress>
 
-                </div>
-                <div class="card">
-                    <img src="../asset/images/arduino.png" alt="logo arduino">
-                
-                    <h3>Arduino</h3>
-                    <p>Création feux circulation, robot reveil, gestion heure, gestion materiel & logiciel</p>
-                    <progress max="100" value="75"></progress>
+                    </div>
+                    
+                    <div class="card">
+                        <img src="../asset/images/arduino.png" alt="logo arduino">
+                    
+                        <h2>Arduino</h2>
+                        <p>
+                            Création de feux de circulation, robot-réveil, gestion de l'heure, gestion matériel et logiciel.
+                        </p>
+                        <progress max="100" value="75"></progress>
 
-                </div>
-                <div class="card">
-                    <img src="../asset/images/java.png" alt="logo java">
-                
-                    <h3>Java</h3>
-                    <p>Implémentation back & front end d'un logiciel</p>
-                    <progress max="100" value="60"></progress>
+                    </div>
+                    
+                    <div class="card">
+                        <img src="../asset/images/java.png" alt="logo java">
+                    
+                        <h2>Java</h2>
+                        <p>
+                            Implémentation front-end et back-end d'un logiciel.
+                        </p>
+                        <progress max="100" value="60"></progress>
 
-                </div>
-                <div class="card">
-                    <img src="../asset/images/solidworks.png" alt="logo solidworks">
-                
-                    <h3>SolidWorks / 3D</h3>
-                    <p>Modelisation & impression piece technique et figurine</p>
-                    <progress max="100" value="60"></progress>
+                    </div>
+                    
+                    <div class="card">
+                        <img src="../asset/images/solidworks.png" alt="logo solidworks">
+                    
+                        <h2>SolidWorks / 3D</h2>
+                        <p>
+                            Modélisation et impression de pièces techniques et figurines.
+                        </p>
+                        <progress max="100" value="60"></progress>
 
-                </div>
-                <div class="card">
-                    <img src="../asset/images/solidworks.png" alt="image qui represente le modélisme">
-                
-                    <h3>Modélisme</h3>
-                    <p>Voiture RC & Drone Fpv à réparer </p>
-                    <progress max="100" value="90"></progress>
+                    </div>
+                    
+                    <div class="card">
+                        <img src="../asset/images/solidworks.png" alt="image qui represente le modélisme">
+                    
+                        <h2>Modélisme</h2>
+                        <p>
+                            Réparation de voitures RC et drones FPV.
+                        </p>
+                        <progress max="100" value="90"></progress>
 
-                </div>
-                <div class="card">
-                    <img src="../asset/images/circuit-electrique.png" alt="image qui represente un circuit electrique">
-                
-                    <h3>Conception schéma electrique</h3>
-                    <p>creation de schéma electrique + realisation schéma electrique</p>
-                    <progress max="100" value="80"></progress>
+                    </div>
+                    
+                    <div class="card">
+                        <img src="../asset/images/circuit-electrique.png" alt="image qui represente un circuit electrique">
+                    
+                        <h2>Conception schéma electrique</h2>
+                        <p>
+                            Création et réalisation de schémas électriques pour divers projets.
+                        </p>
+                        <progress max="100" value="80"></progress>
 
+                    </div>
                 </div>
-            </div>
-           
-        </section>
-    </div> 
-    
-    <?php include __DIR__ . '/../affichage/footer.php';?>
+            
+            </section>
+        </div> 
+        
+        <?php include __DIR__ . '/../affichage/footer.php';?>
 
-</body>
+    </body>
 </html>

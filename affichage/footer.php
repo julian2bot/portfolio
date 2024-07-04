338520d9c01303e7b42fa311640526b7c71c6af6
julian2bot <marquesjulian26@gmail.com>
2024-07-04 22:07:35 +0200

    footer + about me + reglé probleme header media query

--- affichage/footer.php
+++ affichage/footer.php
@@ -1,4 +1,26 @@
 <footer>
-    <p>soon...</p>
-
+    <div class="footer-container">
+        <div class="social-icons">
+            <a href="https://www.instagram.com/julian_fpv_" target="_blank">
+                <img src="../asset/images/instagram.png" alt="Instagram">
+            </a>
+            <a href="https://github.com/julian2bot" target="_blank">
+                <img src="../asset/images/github.png" alt="GitHub">
+            </a>            
+            <!-- <a href="https://github.com/julian1bot" target="_blank">
+                <img src="../asset/images/oldGithub.png" alt="GitHub">
+            </a> -->
+            <a href="https://discordapp.com/users/361166463878561803" target="_blank">
+                <img src="../asset/images/discord.png" alt="Discord">
+            </a>
+        </div>
+        <div>
+            <p>JULIAN MARQUES</p>
+            <p>PORTFOLIO SITE WEB © 2024</p>
+        </div>
+        <div class="contact-info">
+            <p>Téléphone : <a href="tel:+33123456789">+33 1 23 45 67 89</a></p>
+            <p>Email : <a href="mailto:marquesjulian26@gmail.com">marquesjulian26@gmail.com</a></p>
+        </div>
+    </div>
 </footer>

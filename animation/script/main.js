f12eda723dbfaaaca0d534ddb8c17e3672c2b9fb
julian2bot <marquesjulian26@gmail.com>
2024-07-05 02:23:05 +0200

    cv affichage pdf fait

--- animation/script/main.js
+++ animation/script/main.js
@@ -26,3 +26,26 @@ document.querySelectorAll('.nav-header').forEach(link => {
 
 
 
+document.querySelectorAll('.cv').forEach(link => {
+    link.addEventListener('click', function(event) {
+        event.preventDefault(); 
+
+        document.getElementById('pdf-viewer').style.display = "flex";
+        document.querySelector('.body').style.filter = 'blur(2px)';
+        document.body.style.overflowY = 'hidden';
+    });
+});
+
+
+
+document.querySelectorAll('.close').forEach(link => {
+    link.addEventListener('click', function(event) {
+        event.preventDefault(); 
+
+        document.getElementById('pdf-viewer').style.display = "none";
+        
+        document.querySelector('.body').style.filter = 'blur(0px)';
+        document.body.style.overflowY = 'scroll';
+
+    });
+});

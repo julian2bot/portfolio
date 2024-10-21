e7b8ce7ed8cd7bf3c6810f7fa8babf1bb6e2fae4
julian-marques <juliandronefpv@gmail.com>
2024-10-21 08:13:01 +0200

    dark mode + chnagement durée animation

--- animation/script/script.js
+++ animation/script/script.js
@@ -81,6 +81,10 @@ if (document.readyState === 'complete' || document.readyState === 'interactive')
                 input_.classList.add('white-theme');
                 input_.classList.remove('dark-theme');
             }     
+            for (let textarea_ of textarea) {
+                textarea_.classList.add('white-theme');
+                textarea_.classList.remove('dark-theme');
+            }     
 
             themeButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-moon"><path d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z"/></svg>';
 
@@ -104,7 +108,10 @@ if (document.readyState === 'complete' || document.readyState === 'interactive')
                 input_.classList.add('dark-theme');
                 input_.classList.remove('white-theme');
             }
-
+            for (let textarea_ of textarea) {
+                textarea_.classList.add('dark-theme');
+                textarea_.classList.remove('white-theme');
+            }   
             themeButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-sun"><circle cx="12" cy="12" r="4"/><path d="M12 2v2"/><path d="M12 20v2"/><path d="m4.93 4.93 1.41 1.41"/><path d="m17.66 17.66 1.41 1.41"/><path d="M2 12h2"/><path d="M20 12h2"/><path d="m6.34 17.66-1.41 1.41"/><path d="m19.07 4.93-1.41 1.41"/></svg>';
         }
     }
